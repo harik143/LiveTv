b9eaf5e50f2fec9bcb5f34c8395611b85d8cf03b
harik143 <112818162+harik143@users.noreply.github.com>
2024-03-16 12:15:51 +0530

    Update b1g.php

--- b1g.php
+++ b1g.php
@@ -1,19 +1,11 @@
 <?php
-function convertUrl($url) {
-    $parsed_url = parse_url($url);
-    
-    // Extract the parts of the original URL
-    $path_segments = explode('/', $parsed_url['path']);
-    $channel_id = $path_segments[2];
-    
-    // Construct the new URL
-    $new_url = 'https://harik143.github.io/LiveTv/b1g.php?id=' . $channel_id;
-    
-    return $new_url;
-}
+// Get the id parameter from the URL
+$id = $_GET['id'];
 
-// Example usage
-$original_url = 'https://b1g.ooo:80/live/98765432/98765432/';
-$new_url = convertUrl($original_url);
-echo $new_url;
+// Construct the new URL
+$new_url = "https://b1g.ooo:80/live/98765432/98765432/$id";
+
+// Redirect to the new URL
+header("Location: $new_url");
+exit();
 ?>
