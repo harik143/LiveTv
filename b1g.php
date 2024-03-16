12cde4e9b6b8df11503d2d8d5825cc07bbc625b6
harik143 <112818162+harik143@users.noreply.github.com>
2024-03-16 13:03:17 +0530

    Update b1g.php

--- b1g.php
+++ b1g.php
@@ -1,9 +1,6 @@
 <?php
-// Get the id parameter from the URL
-$id = $_GET['id'];
-
-// Construct the original URL based on the ID
-$original_url = "https://ardinesh-pc-remote.puspas.com.np/live-tv/dittotvv/live/$id/index.m3u8";
+// Define the original URL
+$original_url = "https://ardinesh-pc-remote.puspas.com.np/live-tv/dittotvv/live/maa-gold/index.m3u8";
 
 // Redirect to the original URL
 header("Location: $original_url");
