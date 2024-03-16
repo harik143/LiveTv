406ad0bceca5160349e5c0d196dc4c979580911c
harik143 <112818162+harik143@users.noreply.github.com>
2024-03-16 12:36:51 +0530

    Update b1g.php

--- b1g.php
+++ b1g.php
@@ -5,12 +5,7 @@ $id = $_GET['id'];
 // Construct the original URL
 $original_url = "http://livetvbox.live:8080//live/Michelle123/Michelle123/$id.ts";
 
-// Read the content of the original URL
-$content = file_get_contents($original_url);
-
-// Set the appropriate content type
-header("Content-Type: video/mp2t");
-
-// Output the content
-echo $content;
+// Redirect to the original URL
+header("Location: $original_url");
+exit();
 ?>
